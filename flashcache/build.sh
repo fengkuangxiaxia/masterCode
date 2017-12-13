5011b4805030086e1217e7efd8e1cf670a41ce33
傅雨东 <fengkuangxiaxia@sjtu.edu.cn>
2017-12-13 13:48:45 +0800

    系统测试

--- flashcache/build.sh
+++ flashcache/build.sh
@@ -2,5 +2,5 @@ rmmod flashcache
 make clean
 make KERNEL_TREE=/usr/src/linux-3.10.108/
 insmod src/flashcache.ko request_based=1
-src/utils/flashcache_create -p back cachedev /dev/sdc /dev/sde
+src/utils/flashcache_create -p back cachedev /dev/sdb /dev/sdi
 echo cfq > /sys/block/dm-2/queue/scheduler
